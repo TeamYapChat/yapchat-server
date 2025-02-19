7aedcb30df3dbe7771235e6bf87ad7f80553eec9
KhiemNguyen15 <khiemsa15@gmail.com>
2025-02-18 20:47:37 -0600

    chore: Create app level env variable

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,12 +2,14 @@ package config
 
 import (
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/log"
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
+	AppEnv           string // "prod", "dev", "test"
 	DBHost           string
 	DBUser           string
 	DBPassword       string
@@ -17,43 +19,52 @@ type Config struct {
 	EmailTemplateID  string
 }
 
-func LoadConfig() (config Config) {
-	// Test environment
-	if err := godotenv.Load(); err != nil {
-		log.Warn("No .env file found.")
-	} else {
-		return Config{
-			DBHost:           os.Getenv("DB_HOST"),
-			DBUser:           os.Getenv("DB_USER"),
-			DBPassword:       os.Getenv("DB_PASS"),
-			DBName:           os.Getenv("DB_NAME"),
-			JWTSecret:        os.Getenv("JWT_SECRET"),
-			MailerSendAPIKey: os.Getenv("MAILERSEND_API_KEY"),
-			EmailTemplateID:  os.Getenv("EMAIL_TEMPLATE_ID"),
+func LoadConfig() Config {
+	var config Config
+
+	config.AppEnv = strings.ToLower(os.Getenv("APP_ENV"))
+	if config.AppEnv == "" {
+		config.AppEnv = "prod" // Default to prod
+	}
+
+	if config.AppEnv == "dev" || config.AppEnv == "test" {
+		if err := godotenv.Load(); err != nil {
+			log.Warn("No .env file found. Using environment variables directly.")
 		}
 	}
 
-	// Production environment
-	config = Config{
-		DBHost:           os.Getenv("DB_HOST"),
-		DBUser:           os.Getenv("DB_USER"),
-		DBPassword:       getSecret("DB_PASS_FILE"),
-		DBName:           os.Getenv("DB_NAME"),
-		JWTSecret:        getSecret("JWT_SECRET_FILE"),
-		MailerSendAPIKey: getSecret("MAILERSEND_API_KEY_FILE"),
-		EmailTemplateID:  os.Getenv("EMAIL_TEMPLATE_ID"),
+	config.DBHost = os.Getenv("DB_HOST")
+	config.DBUser = os.Getenv("DB_USER")
+	config.DBName = os.Getenv("DB_NAME")
+	config.JWTSecret = os.Getenv("JWT_SECRET")
+	config.EmailTemplateID = os.Getenv("EMAIL_TEMPLATE_ID")
+
+	if config.AppEnv == "prod" {
+		config.DBPassword = getSecret("DB_PASS_FILE")
+		config.MailerSendAPIKey = getSecret("MAILERSEND_API_KEY_FILE")
 	}
 
-	return
+	return config
 }
 
 func getSecret(envName string) string {
 	secretPath := os.Getenv(envName)
+	if secretPath == "" {
+		log.Fatal("Environment variable for secret file path not set", "env", envName)
+	}
 
 	data, err := os.ReadFile(secretPath)
 	if err != nil {
-		log.Fatal("Environment variable not set", "env", envName, "err", err.Error())
+		log.Fatal(
+			"Failed to read secret file",
+			"path",
+			secretPath,
+			"env",
+			envName,
+			"err",
+			err.Error(),
+		)
 	}
 
-	return string(data)
+	return strings.TrimSpace(string(data))
 }
