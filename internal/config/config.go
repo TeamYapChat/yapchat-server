7ffec900b8171bd1d612887074c0e985f0fe05b8
KhiemNguyen15 <khiemsa15@gmail.com>
2025-02-24 09:46:44 -0600

    chore: Switch to environment variables over secret files

--- internal/config/config.go
+++ internal/config/config.go
@@ -40,36 +40,8 @@ func LoadConfig() Config {
 	config.DBName = os.Getenv("DB_NAME")
 	config.JWTSecret = os.Getenv("JWT_SECRET")
 	config.EmailTemplateID = os.Getenv("EMAIL_TEMPLATE_ID")
-
-	if config.AppEnv == "prod" {
-		config.DBPassword = getSecret("DB_PASS_FILE")
-		config.MailerSendAPIKey = getSecret("MAILERSEND_API_KEY_FILE")
-	} else {
-		config.DBPassword = os.Getenv("DB_PASS")
-		config.MailerSendAPIKey = os.Getenv("MAILERSEND_API_KEY")
-	}
+	config.DBPassword = os.Getenv("DB_PASS")
+	config.MailerSendAPIKey = os.Getenv("MAILERSEND_API_KEY")
 
 	return config
-}
-
-func getSecret(envName string) string {
-	secretPath := os.Getenv(envName)
-	if secretPath == "" {
-		log.Fatal("Environment variable for secret file path not set", "env", envName)
-	}
-
-	data, err := os.ReadFile(secretPath)
-	if err != nil {
-		log.Fatal(
-			"Failed to read secret file",
-			"path",
-			secretPath,
-			"env",
-			envName,
-			"err",
-			err.Error(),
-		)
-	}
-
-	return strings.TrimSpace(string(data))
 }
