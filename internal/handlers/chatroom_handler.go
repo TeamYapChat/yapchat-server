71172770d227b4a8b63eade0efd8f02a54abf180
KhiemNguyen15 <khiemsa15@gmail.com>
2025-04-11 09:31:07 -0500

    fix: Allow chat room fetching with invite code

--- internal/handlers/chatroom_handler.go
+++ internal/handlers/chatroom_handler.go
@@ -107,7 +107,21 @@ func (h *ChatRoomHandler) GetByIDHandler(c *gin.Context) {
 		return
 	}
 
-	if !slices.Contains(getParticipantIDs(chatroom.Participants), userID.(string)) {
+	inviteCode := c.Query("code")
+	if inviteCode != "" {
+		_, err = h.chatroomService.GetByInviteCode(inviteCode)
+		if err != nil {
+			if errors.Is(err, services.ErrChatRoomNotFound) {
+				c.JSON(http.StatusNotFound, utils.NewErrorResponse("Chat room not found"))
+			} else {
+				c.JSON(http.StatusInternalServerError, utils.NewErrorResponse("Failed to get chat room"))
+			}
+			return
+		}
+	}
+
+	if !slices.Contains(getParticipantIDs(chatroom.Participants), userID.(string)) &&
+		inviteCode == "" {
 		c.JSON(http.StatusForbidden, utils.NewErrorResponse("User not in chat room"))
 		return
 	}
